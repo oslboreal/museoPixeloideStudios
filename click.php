256bb49240c6071b7dbddfec4cafce9c07dccf30
Vallejo Juan Marcos <oslboreal@gmail.com>
2017-12-25 15:24:33 -0300

    Add files via upload

--- click.php
+++ click.php
@@ -1,36 +1,68 @@
 <?php
 class Click
 {
+    public $id;
     public $pantalla;
     public $boton;
-    public $latitud;
-    public $longitud;
+    public $lat;
+    public $lng;
     public $fecha;
 
-    public function Click($pantalla, $boton, $latitud, $longitud)
-    {
-        $this->fecha = date("Y-m-d H:i:s");
-        $this->pantalla = $pantalla;
-        $this->boton = $boton;
-        $this->latitud = $latitud;
-        $this->longitud = $longitud;
-    }
-
-    public function dbGuardarClick()
+    /*
+    Método guardar clicks: Se encarga de almacenar los clicks en la base de datos.
+    */
+    static public function dbGuardarClick($pantalla, $boton, $latitud, $longitud)
     {
         try
         {
             // Accedemos a las bibliotecas AccesoDatos.
             $datos = AccesoDatos::dameUnObjetoAcceso();
+            $fecha = date("Y-m-d H:i:s");
             //id	fecha	lat	lng	pantalla boton
-                $consulta =$datos->RetornarConsulta("INSERT into tablaclicks (fecha,lat,lng,pantalla,boton)".
-                "values('$this->fecha','$this->latitud','$this->longitud','$this->pantalla','$this->boton')");
+                $consulta = $datos->RetornarConsulta("INSERT into tablaclicks (fecha,lat,lng,pantalla,boton)".
+                "values('$fecha','$latitud','$longitud','$pantalla','$boton')");
                 $consulta->execute();
             return true;
         }catch(Exception $e)
         {
             echo 'Excepción capturada - Error guardando datos de Click: ',  $e->getMessage(), "\n";
         }    
+    }
+
+    /*
+    Método traer clicks: Se encarga de traer clicks en la pantalla especificada en una distancia especificada con un $changui
+    A partir de la latitud y la longitud establecida con un rango de fecha especificado. 
+    */
+    static public function traerClicks($pantalla, $latitud, $longitud, $changui, $fechaDesde, $fechaHasta)
+    {
+        try
+        {
+            $objetoAcceso = AccesoDatos::dameUnObjetoAcceso();
+            echo "Filtrando desde " . $fechaDesde . " hasta " . $fechaHasta;
+            $consulta = $objetoAcceso->RetornarConsulta("SELECT * FROM tablaclicks WHERE lat <= $latitud + $changui AND lat >= $latitud - $changui AND lng <= $longitud + $changui AND lng >= $longitud - $changui AND pantalla = $pantalla AND fecha >= '$fechaDesde' AND fecha <= '$fechaHasta'");
+            $consulta->execute();
+            return $consulta->fetchAll(PDO::FETCH_CLASS,"Click");
+        }catch(Exception $e)
+        {
+            echo 'Excepción capturada - Trayendo datos de los Clicks.: ',  $e->getMessage(), "\n";
+        }   
+    }
+    /*
+    Método traer todos los clicks: Trae todos los clicks de la base de datos.
+    */
+    static public function traerClicksTodos()
+    {
+        try
+        {
+            $objetoAcceso = AccesoDatos::dameUnObjetoAcceso();
+            echo "Filtrando desde " . $fechaDesde . " hasta " . $fechaHasta;
+            $consulta = $objetoAcceso->RetornarConsulta("SELECT * FROM tablaclicks");
+            $consulta->execute();
+            return $consulta->fetchAll(PDO::FETCH_CLASS,"Click");
+        }catch(Exception $e)
+        {
+            echo 'Excepción capturada - Trayendo datos de los Clicks.: ',  $e->getMessage(), "\n";
+        }   
     }
 }
 ?>
